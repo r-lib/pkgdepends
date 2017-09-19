2e04df5a45c68c4076aa59356a1ff10ae563b62c
Gábor Csárdi <csardi.gabor@gmail.com>
2017-09-19 09:19:12 +0100

    Document remotes class

--- R/remotes.R
+++ R/remotes.R
@@ -1,5 +1,94 @@
 
+#' Class for package dependency resolution and package downloads
+#'
+#' @section Usage:
+#' ```
+#' r <- remotes$new(specs, config = list())
+#'
+#' r$resolve()
+#' r$async_resolve()
+#' r$get_resolution()
+#'
+#' r$download()
+#' r$async_download()
+#' r$get_download_status()
+#'
+#' @section Arguments:
+#' * `specs`: Package specifications. See 'Remote Types' below.
+#' * `config`: Custom configuration, a named list. See the list of options
+#'   below.
+#'
+#' @section Details:
+#'
+#' `$new()` creates a new resolution/download task. The packages must be
+#' supplied.
+#'
+#' `$resolve()` resolves all remote packages, and their dependencies.
+#' The result of the resolution is stored internally in the `remotes`
+#' object, and also returned. It can be queried later with
+#' `$get_resolution()`. See `Resolution Table` below.
+#'
+#' `$async_resolve()` returns a deferred value for the resolution of
+#' all remote packages. The deferred value resolves to the same value as
+#' returned by `$resolve()`.
+#'
+#' `$get_resolution()` returns the result of the resolution. See
+#' `Resolution Table` below.
+#'
+#' @section Remote types:
+#' The following remote types are currently supported:
+#'
+#' * CRAN packages: 
+#'     \preformatted{
+#'     [cran::]<package>[@[>=]<version> | @current | @last]
+#'     }
+#' * Packages from GitHub repositories
+#'     \preformatted{
+#'     [<package>=][github::]<username>/<repo>[/<subdir>]
+#'     [@committish | #<pull> | @[*]release]
+#'     }
+#'
+#' @section Resolution table:
+#'
+#' The data frame returned by `$resolve()` et al. has the following
+#' columns:
+#' * `ref` The original remote reference.
+#' * `direct` Whether the reference was specified (`TRUE`), or it is a
+#'   dependency (`FALSE`).
+#' * `status` Status of the resolution: `"OK"` or `"FAILED"`.
+#' * `package` Name of the package.
+#' * `version` Package version.
+#' * `platform` Platform: `"source"`, `"macos"` or `"windows"` currently.
+#' * `rversion` If it is a binary package, which R version it was built for.
+#'   For source packages `"*"` is used.
+#' * `repodir` The relative directory of the package file in the repository.
+#' * `sources` URLs to download the package from. CRAN URLs are not stable,
+#'   so each row typically contains multiple URLs here.
+#' * `target` Relative path for the package file in the repository.
+#' * `fulltarget` Fulle path for the package file in the cache.
+#'
+#' @section Configuration options:
+#' * `cache_dir` Path to the cache. By default a temporary directory is
+#'   used. (TODO)
+#' * `platforms` The platforms to use. Defaults to the current platform
+#'   on Windows and macOS and source.
+#' * `cran-mirror` The CRAN mirror to use. Defaults to `getOption("repos")`
+#'   or the cloud mirror if not set.
+#' * `dependencies` Dependencies to install. Defaults to the *hard*
+#'   dependencies: `Depends`, `Imports`, `LinkingTo`.
+#' * `r-versions`: R version to support, for the binary packages. Defaults
+#'   to the running R version.
+#'
 #' @importFrom R6 R6Class
+#' @import async
+#' @name remotes
+#' @examples
+#' rems <- remotes$new(c("dplyr", "r-lib/rcmdcheck"))
+#' rems$resolve()
+#' rems$download()
+#' rems$get_download_status()
+NULL
+
 #' @export
 
 remotes <- R6Class(

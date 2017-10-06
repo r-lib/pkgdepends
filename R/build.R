dc3617e388f6326be136260905be5747611ab378
Gábor Csárdi <csardi.gabor@gmail.com>
2017-10-06 18:43:22 +0100

    Use pkgbuild to build packages, closes #30

--- R/build.R
+++ R/build.R
@@ -1,23 +1,12 @@
 
-#' @importFrom withr with_dir
-#' @importFrom callr rcmd_safe
+#' @importFrom pkgbuild build
+#' @importFrom utils modifyList
 
-build_package <- function(path) {
-
-  path <- normalizePath(path)
-  pkgdir <- dirname(path)
-
-  ## If not a tar.gz, build it. Otherwise just leave it as it is.
-  build_status <- with_dir(
-    pkgdir,
-    rcmd_safe("build", basename(path))
+build_package <- function(path, build_args = list()) {
+  default_args <- list(
+    path = path, dest_path = NULL, binary = FALSE, vignettes = TRUE,
+    manual = TRUE, args = NULL, quiet = TRUE
   )
-  if (build_status$status != 0) {
-    stop("Build failed for ", sQuote(basename(path)))
-  }
-
-  file.path(
-    pkgdir,
-    list.files(pkgdir, pattern = "\\.tar\\.gz$")
-  )
+  args <- modifyList(default_args, build_args)
+  do.call(build, args)
 }
--- R/onload.R
+++ R/onload.R
@@ -1,6 +1,10 @@
 
+## nocov start
+
 cran_metadata_cache <- NULL
 
 .onLoad <- function(libname, pkgname) {
   cran_metadata_cache <<- CRANMetadataCache$new()
 }
+
+## nocov end

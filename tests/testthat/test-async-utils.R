82b109a4a8b35687cfa79e20e1a23d5cc4ecf3a8
Gábor Csárdi <csardi.gabor@gmail.com>
2017-11-24 11:43:19 +0000

    Additional test cases for utils

--- tests/testthat/test-async-utils.R
+++ tests/testthat/test-async-utils.R
@@ -93,6 +93,29 @@ test_that("download_if_newer, matching etag", {
   expect_equal(readLines(etag), "foobar")
 })
 
+test_that("download_if_newer, error", {
+
+  skip_if_offline()
+
+  cat("dummy\n", file = target <- tempfile())
+
+  expect_error(
+    synchronise(download_if_newer(
+      url <- "http://0.42.42.42",
+      target = target
+    )),
+    class = c("async_rejected", "async_http_error")
+  )
+
+  expect_error(
+    synchronise(download_if_newer(
+      url <- "https://httpbin.org/status/404",
+      target = target
+    )),
+    class = c("async_rejected", "async_http_404", "async_http_error")
+  )
+})
+
 test_that("download_try_list", {
 
   skip_if_offline()
@@ -106,6 +129,49 @@ test_that("download_try_list", {
 
   res <- jsonlite::fromJSON(readLines(tmp), simplifyVector = FALSE)
   expect_equal(res$args$q, "1")
+})
+
+test_that("download_try_list, etag", {
+
+  skip_if_offline()
+
+  dir.create(dir <- tempfile())
+
+  cat("eeeetag\n", file = etag <- file.path(dir, "etag"))
+  dx <- synchronise(download_try_list(
+    c("https://httpbin.org/status/404",
+      "https://httpbin.org/status/403",
+      url <- "https://httpbin.org/etag/foobar"),
+    target <- file.path(dir, "file1"),
+    etag_file = etag
+  ))
+
+  expect_true(file.exists(target))
+  expect_equal(jsonlite::fromJSON(target)$url, url)
+  expect_true(file.exists(etag))
+  expect_equal(readLines(etag), "foobar")
+})
+
+test_that("download_try_list, matching etag", {
+
+  skip_if_offline()
+
+  dir.create(dir <- tempfile())
+
+  cat("foobar\n", file = etag <- file.path(dir, "etag"))
+  cat("dummy\n", file = target <- file.path(dir, "file1"))
+  dx <- synchronise(download_try_list(
+    c("https://httpbin.org/status/404",
+      "https://httpbin.org/status/403",
+      url <- "https://httpbin.org/etag/foobar"),
+    target,
+    etag_file = etag
+  ))
+
+  expect_true(file.exists(target))
+  expect_equal(readLines(target), "dummy")
+  expect_true(file.exists(etag))
+  expect_equal(readLines(etag), "foobar")
 })
 
 test_that("download_try_list, errors", {
@@ -126,4 +192,17 @@ test_that("download_try_list, errors", {
   err <- tryCatch(synchronise(afun()), error = identity)
   expect_match(conditionMessage(err), "All URLs failed")
   expect_false(file.exists(tmp))
+})
+
+test_that("get_async_value", {
+  expect_identical(get_async_value(NULL), NULL)
+  expect_identical(get_async_value(1:10), 1:10)
+
+  afun <- async(function() {
+    v <- async::async_constant(42)
+    await(v)
+    expect_identical(get_async_value(v), 42)
+  })
+
+  synchronise(afun())
 })

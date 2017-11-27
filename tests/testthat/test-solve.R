8474762f663687b91843e0bfbe560e2f7a56c9b9
Gábor Csárdi <csardi.gabor@gmail.com>
2017-11-27 12:38:02 +0000

    Some more solve() tests

--- tests/testthat/test-solve.R
+++ tests/testthat/test-solve.R
@@ -5,6 +5,7 @@ test_that("binary preferred over source", {
   pkgs <- read_fixture("resolution-simple.rds")
   lp <- remotes_i_create_lp_problem(pkgs)
   sol <- remotes_i_solve_lp_problem(lp)
+  expect_equal(sol$status, 0)
   expect_identical(as.logical(sol$solution), c(TRUE, FALSE))
 })
 
@@ -12,5 +13,26 @@ test_that("installed preferred over download", {
   pkgs <- read_fixture("resolution-installed.rds")
   lp <- remotes_i_create_lp_problem(pkgs)
   sol <- remotes_i_solve_lp_problem(lp)
+  expect_equal(sol$status, 0)
   expect_identical(as.logical(sol$solution), c(TRUE, FALSE, FALSE))
+})
+
+test_that("dependency versions are honored", {
+  pkgs <- make_fake_resolution(
+    `pkgA@1.0.0` = list(version = "1.0.0"),
+    `pkgA@2.0.0` = list(version = "2.0.0"),
+    pkgB = list(deps = make_fake_deps(Imports = "pkgA (>= 2.0.0)"))
+  )
+
+  lp <- remotes_i_create_lp_problem(pkgs)
+  sol <- remotes_i_solve_lp_problem(lp)
+  expect_equal(sol$status, 0)
+  expect_identical(as.logical(sol$solution), c(FALSE, TRUE, TRUE))
+})
+
+test_that("conflict: different versions required for package", {
+  pkgs <- read_fixture("resolution-gh-vs-cran.rds")
+  lp <- remotes_i_create_lp_problem(pkgs)
+  sol <- remotes_i_solve_lp_problem(lp)
+  expect_equal(sol$status, 2)
 })

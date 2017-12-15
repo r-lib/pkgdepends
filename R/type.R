c8b58a1a2a1785034ececa66e024ae723f25ac69
Gábor Csárdi <csardi.gabor@gmail.com>
2017-12-15 14:31:10 +0000

    Need to register S3 methods

--- R/type.R
+++ R/type.R
@@ -31,12 +31,16 @@ satisfies_remote.default <- function(resolution, candidate, config, ...) {
 get_files <- function(x)
   UseMethod("get_files")
 
+#' @export
+
 get_files.remote_resolution <- function(x) {
   x[["files"]]
 }
 
 set_files <- function(x, f)
   UseMethod("set_files")
+
+#' @export
 
 set_files.remote_resolution <- function(x, f) {
   x[["files"]] <- f
@@ -46,6 +50,8 @@ set_files.remote_resolution <- function(x, f) {
 get_status <- function(x)
   UseMethod("get_status")
 
+#' @export
+
 get_status.remote_resolution <- function(x) {
   if (!is.null(st <- x[["status"]])) return(st)
   if (all(vcapply(get_files(x), "[[", "status") == "OK")) "OK" else "FAILED"
@@ -54,12 +60,16 @@ get_status.remote_resolution <- function(x) {
 num_files <- function(x)
   UseMethod("num_files")
 
+#' @export
+
 num_files.remote_resolution <- function(x) {
   length(get_files(x))
 }
 
 get_remote <- function(x)
   UseMethod("get_remote")
+
+#' @export
 
 get_remote.remote_resolution <- function(x) {
   x[["remote"]]
@@ -68,12 +78,16 @@ get_remote.remote_resolution <- function(x) {
 get_ref <- function(x)
   UseMethod("get_ref")
 
+#' @export
+
 get_ref.remote_resolution <- function(x) {
   get_remote(x)$ref
 }
 
 get_error_message <- function(x)
   UseMethod("get_error_message")
+
+#' @export
 
 get_error_message.remote_resolution <- function(x) {
   x[["error"]]$message

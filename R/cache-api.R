28fff4c40041266dbc51f3ea85c4061f96795389
Gábor Csárdi <csardi.gabor@gmail.com>
2017-12-22 14:00:51 +0000

    Document the cache API

--- R/cache-api.R
+++ R/cache-api.R
@@ -1,4 +1,14 @@
 
+#' Functions to query and manipulate the package cache
+#'
+#' `cache_summary()` returns a short summary of the state of the cache,
+#' e.g. the number of files and their total size. It returns a named list.
+#'
+#' @param path Path of the cache. By default the cache directory is in
+#'   `R-pkg`, within the user's cache directory.
+#'   See [rappdirs::user_cache_dir()].
+#'
+#' @rdname cache_api
 #' @export
 
 cache_summary <- function(path = NULL) {
@@ -12,6 +22,9 @@ cache_summary <- function(path = NULL) {
   )
 }
 
+#' `cache_list()` lists all files in the cache. It returns a `tibble`.
+#'
+#' @rdname cache_api
 #' @export
 
 cache_list <- function(path = NULL) {
@@ -19,12 +32,25 @@ cache_list <- function(path = NULL) {
   as_tibble(package_cache$new(path)$list())
 }
 
+#' `cache_find()` finds all files in the cache that match the specified
+#' attributes. It returns a `tibble`.
+#'
+#' @param ... Extra named arguments to select the package file.
+#' @rdname cache_api
 #' @export
 
 cache_find <- function(path = NULL, ...) {
   as_tibble(cache_get_file(path, target = NULL, ...))
 }
 
+#' `cache_get_file()` copied a file out of the cache into the specified
+#' path. If no file is found, then it returns `NULL`. Otherwise it returns
+#' (invisibly) the tibble of all selected files. If multiple
+#' files match the specified attributes, then the first one is copied to
+#' the `target` path.
+#'
+#' @param target Path where the selected file is copied.
+#' @rdname cache_api
 #' @export
 
 cache_get_file <- function(path = NULL, target, ...) {
@@ -32,16 +58,12 @@ cache_get_file <- function(path = NULL, target, ...) {
   invisible(as_tibble(package_cache$new(path)$copy_to(target, ...)))
 }
 
+#' `cache_delete_files()` deletes the selected files from the caches.
+#' Without any arguments, all files are deleted.
+#' @rdname cache_api
 #' @export
 
-cache_delete_file <- function(path = NULL, ...) {
+cache_delete_files <- function(path = NULL, ...) {
   path <- path %||% get_user_cache_dir()$root
   package_cache$new(path)$delete(...)
-}
-
-#' @export
-
-cache_cleanup_all <- function(path = NULL) {
-  path <- path %||% get_user_cache_dir()$root
-  cache_delete_file(path)
 }

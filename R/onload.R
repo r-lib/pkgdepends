125af0f030e59cf40e5aa02d7046f5406c5ddfe9
Gábor Csárdi <csardi.gabor@gmail.com>
2018-01-08 12:47:32 +0000

    Use backports::strrep

--- R/onload.R
+++ R/onload.R
@@ -5,6 +5,7 @@ cran_metadata_cache <- NULL
 
 .onLoad <- function(libname, pkgname) {
   if (requireNamespace("debugme", quietly = TRUE)) debugme::debugme()
+  backports::import(pkgname, "strrep")
   cran_metadata_cache <<- CRANMetadataCache$new()
 }
 

0935a9e30501f174324e10278cc7ea70123d3cad
Gábor Csárdi <csardi.gabor@gmail.com>
2018-01-22 16:23:14 +0000

    solve: prefer installed > binaries > source
    If the version numbers are the same.
    Really fixes #83.

--- R/solve.R
+++ R/solve.R
@@ -125,14 +125,17 @@ remotes_i_create_lp_problem <- function(pkgs, policy) {
 
   } else if (policy == "upgrade") {
     ## Sort the candidates of a package according to version number
-    lp$obj <- rep(num_candidates + 1L, num_candidates)
+    lp$obj <- rep((num_candidates + 1) * 4, num_candidates)
     whpp <- pkgs$status == "OK" & !is.na(pkgs$version)
     pn <- unique(pkgs$package[whpp])
     for (p in pn) {
       whp <-  whpp & pkgs$package == p
       v <- pkgs$version[whp]
-      lp$obj[whp] <- order(package_version(v), decreasing=TRUE)
+      r <- rank(package_version(v), ties.method = "min")
+      lp$obj[whp] <- (max(r) - r + 1) * 4
     }
+    lp$obj <- lp$obj + ifelse(pkgs$type == "installed", 0,
+                       ifelse(pkgs$platform == "source", 2, 1))
 
   } else {
     stop("Unknown version selection policy")
--- tests/testthat/test-solve-policies.R
+++ tests/testthat/test-solve-policies.R
@@ -45,3 +45,67 @@ test_that("newer version if preferred in the dependencies", {
   expect_true(sol$objval < 1000)
   expect_equal(as.logical(sol$solution), c(TRUE, TRUE, FALSE, FALSE))
 })
+
+test_that("binaries are still preferred over source", {
+  pkgs <- make_fake_resolution(
+    `aa` = list(version = "1.0.0", direct = TRUE, platform = "macos"),
+    `cran::aa` = list(version = "1.0.0", platform = "source"),
+    `installed::/tmp/aa` = list(version = "0.9.9")
+  )$data
+  lp <- remotes_i_create_lp_problem(pkgs, policy = "upgrade")
+  sol <- remotes_i_solve_lp_problem(lp)
+  expect_true(sol$objval < 1000)
+  expect_equal(as.logical(sol$solution), c(TRUE, FALSE, FALSE, FALSE))
+
+  pkgs <- make_fake_resolution(
+    `cran::aa` = list(version = "1.0.0", platform = "source"),
+    `aa` = list(version = "1.0.0", direct = TRUE, platform = "macos"),
+    `installed::/tmp/aa` = list(version = "0.9.9")
+  )$data
+  lp <- remotes_i_create_lp_problem(pkgs, policy = "upgrade")
+  sol <- remotes_i_solve_lp_problem(lp)
+  expect_true(sol$objval < 1000)
+  expect_equal(as.logical(sol$solution), c(FALSE, TRUE, FALSE, FALSE))
+})
+
+test_that("installed is still preferred over binaries", {
+  pkgs <- make_fake_resolution(
+    `aa` = list(version = "1.0.0", direct = TRUE, platform = "macos"),
+    `cran::aa` = list(version = "1.0.0", platform = "source"),
+    `installed::/tmp/aa` = list(version = "1.0.0")
+  )$data
+  lp <- remotes_i_create_lp_problem(pkgs, policy = "upgrade")
+  sol <- remotes_i_solve_lp_problem(lp)
+  expect_true(sol$objval < 1000)
+  expect_equal(as.logical(sol$solution), c(FALSE, FALSE, TRUE, FALSE))
+
+  pkgs <- make_fake_resolution(
+    `cran::aa` = list(version = "1.0.0", platform = "source"),
+    `aa` = list(version = "1.0.0", direct = TRUE, platform = "macos"),
+    `installed::/tmp/aa` = list(version = "1.0.0")
+  )$data
+  lp <- remotes_i_create_lp_problem(pkgs, policy = "upgrade")
+  sol <- remotes_i_solve_lp_problem(lp)
+  expect_true(sol$objval < 1000)
+  expect_equal(as.logical(sol$solution), c(FALSE, FALSE, TRUE, FALSE))
+
+  pkgs <- make_fake_resolution(
+    `installed::/tmp/aa` = list(version = "1.0.0"),
+    `cran::aa` = list(version = "1.0.0", platform = "source"),
+    `aa` = list(version = "1.0.0", direct = TRUE, platform = "macos")
+  )$data
+  lp <- remotes_i_create_lp_problem(pkgs, policy = "upgrade")
+  sol <- remotes_i_solve_lp_problem(lp)
+  expect_true(sol$objval < 1000)
+  expect_equal(as.logical(sol$solution), c(TRUE, FALSE, FALSE, FALSE))
+
+  pkgs <- make_fake_resolution(
+    `cran::aa` = list(version = "1.0.0", platform = "source"),
+    `installed::/tmp/aa` = list(version = "1.0.0"),
+    `aa` = list(version = "1.0.0", direct = TRUE, platform = "macos")
+  )$data
+  lp <- remotes_i_create_lp_problem(pkgs, policy = "upgrade")
+  sol <- remotes_i_solve_lp_problem(lp)
+  expect_true(sol$objval < 1000)
+  expect_equal(as.logical(sol$solution), c(FALSE, TRUE, FALSE, FALSE))
+})

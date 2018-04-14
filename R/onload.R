cb407d4e834723a857449f29ec9c403ad6821159
Gábor Csárdi <csardi.gabor@gmail.com>
2018-04-14 13:52:27 +0100

    Remove old metadata cache

--- R/onload.R
+++ R/onload.R
@@ -1,14 +1,11 @@
 
 ## nocov start
 
-cran_metadata_cache <- NULL
-
 #' @importFrom cli cli
 
 .onLoad <- function(libname, pkgname) {
   if (requireNamespace("debugme", quietly = TRUE)) debugme::debugme()
   backports::import(pkgname, "strrep")
-  cran_metadata_cache <<- CRANMetadataCache$new()
 }
 
 ## nocov end

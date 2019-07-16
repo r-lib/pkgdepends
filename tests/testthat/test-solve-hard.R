b4757ddb2f9af79ca830542eb6e22c9fd3efdc0d
Gábor Csárdi <csardi.gabor@gmail.com>
2019-07-16 10:54:57 +0100

    Add a harder dependency problem instance

---
+++ tests/testthat/test-solve-hard.R
@@ -0,0 +1,25 @@
+
+context("hard solve instances")
+
+test_that("hard example, because easch package has many candidates", {
+
+  skip_on_cran()
+  skip("unfinished")
+  
+  lib <- test_temp_dir()
+  install.packages(
+    c("dplyr", "rcmdcheck", "qgraph", "usethis", "ggplot2", "knitr"),
+    lib = lib)
+
+  r <- remotes()$new(
+    c("tidyverse/dplyr", "r-lib/rcmdcheck", "cran/qgraph", "r-lib/usethis",
+      "tidyverse/ggplot2", "yihui/knitr", "r-lib/rray"),
+    lib = lib)
+  r$resolve()
+
+  pkgs <- r$get_resolution()
+  prob <- r$.__enclos_env__$private$create_lp_problem(pkgs, "upgrade")
+  sol <- remotes_i_solve_lp_problem(prob)
+
+  expect_true(sol$objval < 1e6)
+})

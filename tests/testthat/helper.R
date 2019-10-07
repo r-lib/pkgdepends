1195b43cec58a197313c22f9c0c8ff07818110e2
Gábor Csárdi <csardi.gabor@gmail.com>
2019-10-07 12:14:46 +0100

    Make spell check test work
    Haven't fixed the actual typos just yet.

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -80,3 +80,25 @@ test_temp_dir <- function(pattern = "test-dir-", envir = parent.frame()) {
   dir.create(tmp, recursive = TRUE, showWarnings = FALSE)
   normalizePath(tmp)
 }
+
+test_package_root <- function() {
+  x <- tryCatch(
+    rprojroot::find_package_root_file(),
+    error = function(e) NULL)
+
+  if (!is.null(x)) return(x)
+
+  pkg <- testthat::testing_package()
+  x <- tryCatch(
+    rprojroot::find_package_root_file(
+      path = file.path("..", "..", "00_pkg_src", pkg)),
+    error = function(e) NULL)
+
+  if (!is.null(x)) return(x)
+
+  stop("Cannot find package root")
+}
+
+skip_in_covr <- function() {
+  if (Sys.getenv("R_COVR") == "true") skip("In covr")
+}

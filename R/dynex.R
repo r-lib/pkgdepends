547b9a14ce3ff6d5bf397ed9d9ab2863894e8ace
Gábor Csárdi <csardi.gabor@gmail.com>
2019-10-08 13:56:50 +0100

    Set up dynamic examples

---
+++ R/dynex.R
@@ -0,0 +1,33 @@
+
+dynex <- function(code) {
+  code <- sub("\\s+$", "", sub("^\\s+", "", code))
+  code <- gsub("\n\n", "\n", code, fixed = TRUE)
+  lines <- strsplit(code, "\n", fixed = TRUE)[[1]]
+  needs <- which(grepl("^# Needs ", lines))
+  cid <- cut(
+    seq_along(lines),
+    breaks = c(if (! 1 %in% needs) 0, needs, length(lines) + 1L),
+    right = FALSE,
+    include.lowest = TRUE)
+  blocks <- tapply(lines, cid, c)
+
+  drop_trailing_empty_lines <- function(x) {
+    empty <- grepl("^\\s*$", x)
+    wempty <- which(empty)
+    drop <- wempty[wempty > max(!empty)]
+    if (length(drop) > 0) x <- x[-drop]
+    x
+  }
+
+  dynex_block <- function(str) {
+    str <- drop_trailing_empty_lines(str)
+    if (grepl("^# Needs ", str[1])) {
+      call <- sub("^# Needs[^:]*:", "", str[1])
+      omit <- tryCatch(!isTRUE(eval(parse(text = call))), error = function(e) TRUE)
+      if (omit) str <- paste0("#x ", str)
+    }
+    paste(str, collapse = "\n")
+  }
+
+  paste(vapply(blocks, dynex_block, character(1)), collapse = "\n\n")
+}

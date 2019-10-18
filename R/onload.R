7278e053b07b9e12c3dab20513c961936c37a745
Gábor Csárdi <csardi.gabor@gmail.com>
2019-10-18 09:52:15 +0100

    Do not need to hand-import 'deferred'
    This is now fixed in pkgcache.

--- R/onload.R
+++ R/onload.R
@@ -1,12 +1,9 @@
 
 ## nocov start
 
-deferred <- NULL
-
 .onLoad <- function(libname, pkgname) {
   if (requireNamespace("debugme", quietly = TRUE)) debugme::debugme()
   backports::import(pkgname, "strrep")
-  deferred <<- asNamespace("pkgcache")$deferred
 }
 
 ## nocov end

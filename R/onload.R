d4b9b84fdae5347cf0ec4efc997da83bff62cb65
Gábor Csárdi <csardi.gabor@gmail.com>
2019-12-23 14:04:54 +0000

    Use lazyrmd.R for vignettes

--- R/onload.R
+++ R/onload.R
@@ -5,6 +5,11 @@
   if (requireNamespace("debugme", quietly = TRUE)) debugme::debugme()
   backports::import(pkgname, "strrep")
   err$onload_hook()
+  lazyrmd$onload_hook(
+    local = "if-newer",
+    ci = TRUE,
+    cran = FALSE
+  )
 }
 
 ## nocov end

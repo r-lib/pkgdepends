87772eaad8b7039c862254991223af9933adb185
Gábor Csárdi <csardi.gabor@gmail.com>
2020-06-21 17:32:54 +0100

    Fix default_platforms() docs

--- R/platform.R
+++ R/platform.R
@@ -3,25 +3,8 @@ get_platform <- function() {
   .Platform
 }
 
-#' The current R platform for packages
-#'
-#' `current_r_platform()` detects the current platform.
-#' `default_platforms()` prints the default package types that are used
-#' on the current platform. See also ['Configuration'][pkg_config].
-#'
-#' @return `current_r_platform()` returns a string:
-#'   `source`, `macos` or `windows`.
-#'
-#'   `default_platforms()` return a character vector of package types that
-#'   work on the current system. It is a subset of the possible
-#'   `current_r_platform()` return values.
-#'
-#' @rdname default_platforms
-#' @family platform functions
 #' @export
-#' @examples
-#' current_r_platform()
-#' default_platforms()
+#' @rdname default_platforms
 
 current_r_platform <- function() {
   type <- get_platform()$pkgType
@@ -36,8 +19,23 @@ current_r_platform <- function() {
   }
 }
 
+#' The current R platform for packages
+#'
+#' `current_r_platform()` detects the current platform.
+#' `default_platforms()` prints the default package types that are used
+#' on the current platform. See also ['Configuration'][pkg_config].
+#'
+#' @return `current_r_platform()` returns a string:
+#'   `source`, `macos` or `windows`.
+#'
+#'   `default_platforms()` return a character vector of package types that
+#'   work on the current system. It is a subset of the possible
+#'   `current_r_platform()` return values.
 #'
 #' @family platform functions
 #' @export
+#' @examples
+#' current_r_platform()
+#' default_platforms()
 
 default_platforms <- function() unique(c(current_r_platform(), "source"))

9cbef7e59b7b04f6c44b6c0e1dade446af42736a
Gábor Csárdi <csardi.gabor@gmail.com>
2020-06-21 20:32:14 +0100

    Remove unneeded Rmd related stuff

--- R/onload.R
+++ R/onload.R
@@ -4,11 +4,6 @@
 .onLoad <- function(libname, pkgname) {
   if (requireNamespace("debugme", quietly = TRUE)) debugme::debugme()
   err$onload_hook()
-  lazyrmd$onload_hook(
-    local = "if-newer",
-    ci = function() has_asciicast_support() && getRversion() >= "3.3",
-    cran = FALSE
-  )
 }
 
 ## nocov end

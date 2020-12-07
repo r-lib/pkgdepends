03247ca855ee65dac4c6b852d9a127a3c31b44ee
Gábor Csárdi <csardi.gabor@gmail.com>
2020-12-07 12:14:05 +0000

    Add lockfile support

--- R/pkg-installation.R
+++ R/pkg-installation.R
@@ -316,6 +316,28 @@ pkg_installation_proposal <- R6::R6Class(
     },
 
     #' @description
+    #' Create a lock file that contains the information to perform
+    #' the installation later, possibly in another R session.
+    #'
+    #' @details
+    #' Note, since the URLs of CRAN and most CRAN-like repositories change
+    #' over time, in practice you cannot perform the plan of the lock file
+    #' _much_ later. For example, binary packages of older package version
+    #' are removed, and won't be found.
+    #'
+    #' Currently the intended use case of lock files in on CI systems, to
+    #' facilitate caching. The (hash of the) lock file provides a good key
+    #' for caching systems.
+    #'
+    #' @param path Name of the lock file. The default is `pkg.lock` in the
+    #'   current working directory.
+    #' @param version Only version 1 is supported currently.
+
+    create_lockfile = function(path = "pkg.lock", version = 1) {
+      private$plan$export_install_plan(plan_file = path, version = version)
+    },
+
+    #' @description
     #' Draw a tree of package dependencies. It returns a `tree` object, see
     #' [cli::tree()]. Printing this object prints the dependency tree to the
     #' screen.
@@ -404,25 +426,6 @@ pkg_installation_proposal <- R6::R6Class(
     },
 
     #' @description
-    #' Create an installation plan for the downloaded packages.
-    #'
-    #' @return
-    #' An installation plan, see ['Installation plans'][install_plans] for
-    #' the format.
-    #'
-    #' @examplesIf pkgdepends:::is_online()
-    #' pdi <- new_pkg_installation_proposal(
-    #'   "pak",
-    #'   config = list(library = tempfile())
-    #' )
-    #' pdi$resolve()
-    #' pdi$solve()
-    #' pdi$download()
-    #' pdi$get_install_plan()
-
-    get_install_plan = function() private$plan$get_install_plan(),
-
-    #' @description
     #' Format a `pkg_installation_proposal` object, typically for printing.
     #'
     #' @param ... not used currently.
@@ -454,8 +457,8 @@ pkg_installation_proposal <- R6::R6Class(
         if (has_sol) "(use `$show_solution()` to see the packages to install",
         if (has_sol) "(use `$get_solution()` to see the full solution results)",
         if (has_sol && !sol_err) "(use `$draw()` to draw the dependency tree)",
+        if (has_sol) "(use `$create_lockfile()` to write a lock file)",
         if (has_dls) "(use `$get_downloads()` to get download data)",
-        if (has_dls) "(use `$get_install_plan()` to get the installation plan)",
         if (has_dls) "(use `$install()` to install the packages)"
       )
     },
---
+++ R/pkg-lockfile.R
@@ -0,0 +1,129 @@
+
+#' @param lockfile Path to the lock file to use.
+#' @param config Configuration options, a named list. See
+#'  ['Configuration'][pkg_config]. If it does not include `library`, then
+#'  `.libPaths()[1]` is added as `library`.
+#' @param ... Additional arguments, passed to
+#'   [`pkg_installation_plan$new()`](#method-new).
+#' @export
+#' @rdname pkg_installation_plan
+
+new_pkg_installation_plan <- function(lockfile = "pkg.lock",
+                                      config = list(), ...) {
+  config$library = config$library %||% .libPaths()[[1]]
+  pkg_installation_plan$new(lockfile, config = config, ...)
+}
+
+#' R6 class for installation from a lock file
+#'
+#' @description
+#' An installation plan is similar to an installation proposal
+#' (i.e. [pkg_installation_proposal]), but it already contains the solved
+#' dependencies, complete with download URLs.
+#'
+#' @details
+#' Typically you create a `pkg_installation_plan` object with
+#' `new_pkg_installation_plan()` and then call its `$download()` method
+#' to download the packages and then its `$install()` method to install
+#' them.
+#'
+#' @export
+
+pkg_installation_plan <- R6::R6Class(
+  "pkg_installation_plan",
+  inherit = pkg_installation_proposal,
+  public = list(
+
+    #' @description
+    #' Create a new `pkg_installation_plan` object. Consider using
+    #' `new_pkg_installation_plan()` instead of calling the constructor
+    #' directly.
+    #'
+    #' The returned object can be used to download and install
+    #' packages, according to the plan.
+    #'
+    #' @param lockfile Path to the lock file to use.
+    #' @param config Configuration options, a named list. See
+    #'   ['Configuration'][pkg_config]. It needs to include the package
+    #'   library to install to, in `library`.
+    #' @param remote_types Custom remote ref types, this is for advanced
+    #'   use, and experimental currently.
+
+    initialize = function(
+      lockfile = "pkg.lock",
+      config = list(),
+      remote_types = NULL
+      ) {
+      assert_that(is_path(config$library))
+      private$library <- config$library
+      private$plan <- pkg_plan$new(
+        lockfile = lockfile,
+        config = config,
+        library = config$library,
+        remote_types = remote_types
+      )
+    },
+
+    #' @description
+    #' This function is implemented for installation plans, and will error.
+
+    resolve = function() stop("Cannot resolve an installation plan"),
+
+    #' @description
+    #' This function is implemented for installation plans, and will error.
+
+    async_resolve = function() stop("Cannot resolve an installation plan"),
+
+    #' @description
+    #' Installation plans are already solved, and this method will return
+    #' `NA_character_`, always.
+
+    get_solve_policy = function() NA_character_,
+
+    #' @description
+    #' This function is implemented for installation plans, and will error.
+
+    set_solve_policy = function() stop("Cannot solve an installation plan"),
+
+    #' @description
+    #' This function is implemented for installation plans, and will error.
+
+    solve = function() stop("Cannot solve an installation plan"),
+
+    #' @description
+    #' Format a `pkg_installation_plan` object, typically for printing.
+    #'
+    #' @param ... not used currently.
+    #'
+    #' @return
+    #' A character vector, each element should be a line in the printout.
+
+    format = function(...) {
+      refs <- private$plan$get_refs()
+
+      has_dls <- private$plan$has_solution_downloads()
+      dls <- if (has_dls) private$plan$get_solution_download()
+      dls_err <- has_dls && any(dls$status == "Failed")
+
+      c("<pkg_installation_plan>",
+        "+ refs:", paste0("  - ", refs),
+        "+ has solution",
+        if (has_dls) "+ has downloads",
+        if (dls_err) "x has download errors",
+        if (!has_dls) "(use `$download()` to download packages)",
+        "(use `$show_solution()` to see the packages to install",
+        "(use `$get_solution()` to see the full solution results)",
+        "(use `$draw()` to draw the dependency tree)",
+        "(use `$get_downloads()` to get download data)",
+        "(use `$get_install_plan()` to get the installation plan)",
+        "(use `$install()` to install the packages)"
+      )
+    }
+
+  ),
+
+  private = list(
+    plan = NULL,
+    library = NULL
+  )
+)

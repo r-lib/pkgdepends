300059cd399388c475825ff49febd55987aa7b78
Gábor Csárdi <csardi.gabor@gmail.com>
2022-02-16 19:48:46 +0100

    Skip tests on CRAN
    Peace of mind.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,8 @@
-library(testthat)
-library(pkgdepends)
 
-Sys.setenv(R_USER_CACHE_DIR = tempdir())
-test_check("pkgdepends")
+if (Sys.getenv("NOT_CRAN") == "true") {
+  library(testthat)
+  library(pkgdepends)
+
+  Sys.setenv(R_USER_CACHE_DIR = tempdir())
+  test_check("pkgdepends")
+}

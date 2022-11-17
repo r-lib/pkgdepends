3a868c2e1d6b9639af77858a783878d2b579a985
Gábor Csárdi <csardi.gabor@gmail.com>
2022-11-17 16:08:31 +0100

    More robust snapshots on Windows
    Removing the path to `tempdir()` is really tricky.

--- tests/testthat/helper-apps.R
+++ tests/testthat/helper-apps.R
@@ -484,5 +484,6 @@ transform_tempdir <- function(x) {
   x <- sub(normalizePath(tempdir()), "<tempdir>", x)
   x <- sub(normalizePath(tempdir(), winslash = "/"), "<tempdir>", x)
   x <- sub("[\\\\/]file[a-zA-Z0-9]+", "/<tempfile>", x)
+  x <- sub("[A-Z]:.*Rtmp[a-zA-Z0-9]+/", "<tempdir>", x)
   x
 }

836d35a66ba0239937c67725e9f269faeb951a9e
Gábor Csárdi <csardi.gabor@gmail.com>
2023-03-25 14:38:57 +0100

    Test recent solution error description improvements

--- tests/testthat/test-solve-describe.R
+++ tests/testthat/test-solve-describe.R
@@ -37,3 +37,106 @@ test_that("conflicting dependencies downstream", {
   expect_equal(dsc$failure_type, c("dep-failed", "dep-failed", "conflict"))
   expect_snapshot(dsc)
 })
+
+test_that("conflicting dependencies and installed packages", {
+
+  gh <- list(
+    users = list(
+      "cran" = list(
+        repos = list(
+          dm = list(
+            commits = list(
+              list(
+                sha = "96dac7a1cffea51709fa3c53c3424d4047ec279286583b8e1e46c701e98d0954",
+                branch = "main",
+                tag = "1.0.0",
+                files = list(
+                  DESCRIPTION = "Package: dm\nVersion: 1.0.0\nImports: tidyr\n",
+                  NAMESPACE = ""
+                )
+              )
+            )
+          ),
+          dplyr = list(
+            commits = list(
+              list(
+                sha = "b259bcc4a82d28effdc59d3d51d4976b32ce29056ef9da48eeb9424e6eaffa16",
+                branch = "main",
+                tag = "1.0.9",
+                files = list(
+                  DESCRIPTION = "Package: dplyr\nVersion: 1.0.9\n"
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  )
+
+  cran <- dcf("
+    Package: tidyr
+    Version: 1.0.0
+    Imports: dplyr (>= 1.0.10)
+
+    Package: dplyr
+    Version: 1.1.1
+  ")
+
+  fake_gh <- webfakes::local_app_process(gh_app(gh))
+  withr::local_envvar(R_PKG_GITHUB_API_URL = fake_gh$url())
+  setup_fake_apps(cran_repo = cran)
+
+  # -----------------------------------------------------------------------
+  lib <- withr::local_tempdir()
+
+  p <- new_pkg_installation_proposal(
+    c("cran/dm@1.0.0", "cran/dplyr@1.0.9"),
+    config = list(library = lib)
+  )
+  suppressMessages(p$resolve())
+  suppressMessages(p$solve())
+  expect_snapshot(p$get_solution()$failures)
+
+  # -----------------------------------------------------------------------
+  # fake an installed package
+  dir.create(file.path(lib, "tidyr"))
+  writeLines(c(
+    "Package: tidyr",
+    "Version: 1.0.0",
+    "Imports: dplyr (>= 1.0.10)",
+    "Repository: CRAN"
+  ), file.path(lib, "tidyr", "DESCRIPTION"))
+  p <- new_pkg_installation_proposal(
+    c("cran/dm@1.0.0", "cran/dplyr@1.0.9"),
+    config = list(library = lib)
+  )
+  suppressMessages(p$resolve())
+  suppressMessages(p$solve())
+  expect_snapshot(
+    p$get_solution()$failures,
+    transform = function(x) {
+      sub("installed::.*tidyr", "installed::<path>/tidyr", x)
+    }
+  )
+
+  # For the rest we need a better way to explain failures
+  skip("TODO")
+
+  local <- withr::local_tempdir()
+  writeLines(c(
+    "Package: local123",
+    "Version: 1.0.0",
+    "Imports: dm, dplyr",
+    "Remotes: cran/dm@1.0.0, cran/dplyr@1.0.9"
+  ), file.path(local, "DESCRIPTION"))
+
+  # First without installed packages
+  p <- new_pkg_installation_proposal(
+    paste0("local::", local),
+    config = list(library = lib)
+  )
+  suppressMessages(p$resolve())
+  suppressMessages(p$solve())
+
+})

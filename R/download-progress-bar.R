eb3fa597546bf1f91c47f5b5064f29c99eb9b7ca
olivroy <52606734+olivroy@users.noreply.github.com>
2023-09-05 15:32:52 -0400

    Update download-progress-bar.R
    pak/pak#535

--- R/download-progress-bar.R
+++ R/download-progress-bar.R
@@ -385,7 +385,7 @@ pkgplan__done_progress_bar <- function(bar) {
   } else if (err == 0) {
     cli_alert_success(
       paste0(
-        "Downloaded {dld} package{?s} {.size ({bts})}",
+        "Downloaded {dld} package{?s} {.size ({bts})} ",
         if (bar$show_time) "in {.time {dt}}"
       )
     )

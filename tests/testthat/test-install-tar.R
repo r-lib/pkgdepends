273c0dab66e3ae98a31ea93306acd511692c8e85
Gábor Csárdi <csardi.gabor@gmail.com>
2024-10-22 18:31:21 +0200

    Fix tests for external tar option change

--- tests/testthat/test-install-tar.R
+++ tests/testthat/test-install-tar.R
@@ -70,28 +70,28 @@ test_that("eup_get_args", {
 
   expect_equal(
     eup_get_args(opts),
-    c("-x", "-f", path_norm(opts$tarfile), "-C", path_norm(opts$exdir), "-z")
+    c("-x", "-f", path_norm(opts$tarfile), "-C", path_norm(opts$exdir), "-o", "-z")
   )
 
   ## No need to ungzip
   opts$tarfile <- system.file(package = "pkgdepends", "tools", "xxx")
   expect_equal(
     eup_get_args(opts),
-    c("-x", "-f", path_norm(opts$tarfile), "-C", path_norm(opts$exdir))
+    c("-x", "-f", path_norm(opts$tarfile), "-C", path_norm(opts$exdir), "-o")
   )
 
   ## Files are specified
   opts$files <- c("this", "that")
   expect_equal(
     eup_get_args(opts),
-    c("-x", "-f", path_norm(opts$tarfile), "-C", path_norm(opts$exdir), opts$files)
+    c("-x", "-f", path_norm(opts$tarfile), "-C", path_norm(opts$exdir), "-o", opts$files)
   )
 
   ## Do not restore times
   opts$restore_times <- FALSE
   expect_equal(
     eup_get_args(opts),
-    c("-x", "-f", path_norm(opts$tarfile), "-C", path_norm(opts$exdir), "-m", opts$files)
+    c("-x", "-f", path_norm(opts$tarfile), "-C", path_norm(opts$exdir), "-o", "-m", opts$files)
   )
 })
 
